2c35da67e16634f10c2d7335d0e3b5da95a8426c
Allen <836414757@qq.com>
2018-11-01 15:18:43 +0800

    Initial commit

---
+++ app/src/main/java/com/lunger/touchsearch/RootViewGroup.java
@@ -0,0 +1,17 @@
+package com.lunger.touchsearch;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Allen on 2018/11/1 0001.
+ * 布局中的顶级容器
+ */
+
+public class RootViewGroup extends RelativeLayout {
+
+    public RootViewGroup(Context context) {
+        super(context);
+    }
+    
+}
